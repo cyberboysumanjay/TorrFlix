b104ce86bc1c4d35401512d2eb7b745e4ed0c93b
cyberboysumanjay <cyberboysumanjay@gmail.com>
2018-07-12 23:31:57 +0530

    Added streaming from Piratebay and Kickass. Improved Command Line Interface.

--- play.py
+++ play.py
@@ -7,15 +7,27 @@ import colored
 from colored import stylize
 import open_magnet
 import peerflix_play
-def yts():
+query=input("Enter the movie name:\n")
+
+# Handle Streaming
+def handle_stream(magnet_link):
+    print("Do you want to stream the movie?\n")
+    print(stylize("Press 1 for Yes or Press 2 for No",colored.fg("red")))
+    selection=int(input())
+    if selection==1:
+        peerflix_play.peerflix_stream(magnet_link)
+    elif selection==2:
+        print("Ok! Paste the magnet link in your default Torrent Client\n")
+    else:
+        print("Wrong choice! Please Try Again\n")
+        
+def yts_search(query):
     print(stylize("Welcome to YTS Movie Downloader.\n", colored.fg("red")))
-    query=input("Enter the movie name:\n")
     url='https://yts.am/api/v2/list_movies.json?query_term='+query
     print()
     print(stylize("Searching......\n",colored.fg("green")))
     source=requests.get(url).text
     loaded_json= (json.loads(source))
-
     print(stylize(loaded_json['status_message'],colored.fg('red')))
     movie_data=(loaded_json['data'])
     movies=movie_data['movies']
@@ -53,13 +65,65 @@ def yts():
     print(stylize("Your magnet link is copied to your clipboard",colored.fg("yellow")))
     print(magnet_link)
     pyperclip.copy(magnet_link)
-yts()
-print(stylize("Press 1 for Yes or Press 2 for No",colored.fg("red")))
-selection=int(input("Do you want to stream the movie?\n"))
-if selection==1:
-#    open_magnet.open_magnet(magnet_link)
-    peerflix_play.peerflix_stream(magnet_link)
-elif selection==2:
-    print("Ok! Paste the magnet link in your default Torrent Client\n")
+    handle_stream(magnet_link)
+
+def piratebay_search(query):
+    scrapper_api='https://api.scraperapi.com/?key=2500866341976677504461594942127656&url='
+    url=scrapper_api+'https://pirateproxy.mx/search/'+query+'/0/99/0'
+    print("Searching......")
+    source=requests.get(url).text
+    soup=bs(source,'lxml')
+    results=soup.find_all('div',class_='detName')
+    i=1
+    for r in results:
+        print(i,r.text)
+        i=i+1
+    print("Enter the Serial Number of the search item you like to download: ")
+    choice=int(input())
+    print ("Fetching data.....")
+    magnet_results=soup.find_all('a',title='Download this torrent using magnet')
+    a=[]
+    for m in magnet_results:
+        a.append(m['href'])
+    magnet_link=(a[choice-1])
+    print("Magnet Link of your selected choice has been fetched.")
+    pyperclip.copy(magnet_link)
+    print ("Your magnet link is now in your clipboard.")
+    handle_stream(magnet_link)
+
+def kickass_search(query):
+    scrapper_api='https://api.scraperapi.com/?key=2500866341976677504461594942127656&url='
+    base_url="https://kickass.unblocked.vet/usearch/"
+    url=scrapper_api+base_url+query+'/'
+    print("Searching......")
+    source=requests.get(url).text
+    soup=bs(source,'lxml')
+    name=soup.find_all('a', class_="cellMainLink")
+    results=[]
+    i=1
+    for r in name:
+        print(i,r.text)
+        i=i+1
+    print("Enter the Serial Number of the search item you like to download: ")
+    choice=int(input())
+    print ("Fetching data.....")
+    magnet=soup.find_all('a', title="Torrent magnet link")
+    a=[]
+    for m in magnet:
+        a.append(m['href'])
+    magnet_link=(a[choice-1])
+    print("Magnet Link of your selected choice has been fetched.")
+    pyperclip.copy(magnet_link)
+    print ("Your magnet link is now in your clipboard.")
+    handle_stream(magnet_link)
+
+engine=int(input("Select your preferred search engine: \n1: PirateBay\n2: Kickass\n3: YTS\n"))
+if engine==1:
+    piratebay_search(query)
+elif engine==2:
+    kickass_search(query)
+elif engine==3:
+    yts_search(query)
 else:
-    print("Wrong choice! Please Try Again\n")
+    print("Wrong Choice! Try again")
+    quit()
